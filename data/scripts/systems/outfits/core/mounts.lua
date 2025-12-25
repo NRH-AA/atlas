e4c49bfdc8f301fe0de6ab7eefed5b7ecd5292a7
Ranieri Althoff <ranisalt@gmail.com>
2025-12-25 02:59:26 -0300

    refactor(mounts): rename parameter in setWasMounted for clarity

--- data/scripts/systems/outfits/core/mounts.lua
+++ data/scripts/systems/outfits/core/mounts.lua
@@ -46,8 +46,8 @@ local wasMounted = {}
 function Player.getWasMounted(self)
     return wasMounted[self:getId()] or false
 end
-function Player.setWasMounted(self, value)
-    wasMounted[self:getId()] = value or nil
+function Player.setWasMounted(self, mounted)
+    wasMounted[self:getId()] = mounted or nil
 end
 
 function Player.isMounted(self)
