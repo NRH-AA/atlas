3ad3304ae939147f79a3f343a3bce422c7b4eb73
Ranieri Althoff <ranisalt@gmail.com>
2026-01-08 00:27:58 -0300

    fix(fluids): fix distillery item check in action.onUse function

--- data/scripts/actions/others/fluids.lua
+++ data/scripts/actions/others/fluids.lua
@@ -66,9 +66,8 @@ function action.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		local fluidSource = targetItemType and targetItemType:getFluidSource() or FLUID_NONE
 		if fluidSource ~= FLUID_NONE then
 			item:transform(item:getId(), fluidSource)
-		elseif table.contains(distillery, item.itemid) then
-			local targetItem = distillery[target.itemid]
-			if targetItem then
+		elseif table.contains(distillery, target.itemid) then
+			if distillery[target.itemid] ~= nil then
 				item:transform(item:getId(), FLUID_NONE)
 			else
 				player:sendCancelMessage("You have to process the bunch into the distillery to get rum.")
